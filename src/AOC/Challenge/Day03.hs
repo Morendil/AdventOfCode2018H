67b54294d811af089a5a376b4f888e21186a7109
Laurent Bossavit <laurent.git@bossavit.com>
2020-01-01 13:43:53 +0100

    Day 3 both parts

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -4,40 +4,51 @@
 -- |
 -- Module      : AOC.Challenge.Day03
 -- License     : BSD3
---
--- Stability   : experimental
--- Portability : non-portable
---
--- Day 3.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day03 (
-    -- day03a
-  -- , day03b
+    day03a
+  , day03b
+  , Claim(..)
+  , overlap
   ) where
 
 import           AOC.Prelude
+import Text.ParserCombinators.ReadP
+import qualified Data.Set as Set
 
-day03a :: _ :~> _
+type Position = (Int, Int)
+type Extent = (Int, Int)
+data Claim = Claim { claimId :: Int, pos :: Position, ext :: Extent} deriving (Eq, Show)
+
+parseClaim = do
+  string "#"
+  id <- int
+  string " @ "
+  px <- int
+  comma
+  py <- int
+  string ": "
+  w <- int
+  string "x"
+  h <- int
+  return $ Claim id (px, py) (w, h)
+
+overlap :: Claim -> Claim -> [Position]
+overlap (Claim id1 (x1,y1) (w1,h1)) (Claim id2 (x2,y2) (w2,h2)) = [(x,y) | 
+  x <- [(max x1 x2)..(min (x1+w1) (x2+w2))-1],
+  y <- [(max y1 y2)..(min (y1+h1) (y2+h2))-1]
+  ]
+
+day03a :: [Claim] :~> Int
 day03a = MkSol
-    { sParse = Just
+    { sParse = parseMaybe (sepBy1 parseClaim (string "\n"))
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . Set.size . Set.fromList . concat . concat . crossWith overlap
     }
 
-day03b :: _ :~> _
+day03b :: [Claim] :~> Int
 day03b = MkSol
-    { sParse = Just
+    { sParse = parseMaybe (sepBy1 parseClaim (string "\n"))
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . claimId . head . map (fst.fromJust) . concat . filter (all isJust) . crossWith (\a b -> (a,b) <$ guard (null $ overlap a b))
     }
--- src/AOC/Common.hs
+++ src/AOC/Common.hs
@@ -8,8 +8,61 @@
 -- Meant to be a place to include common functionality used across
 -- different parts in the challenge.
 --
+module AOC.Common where
 
+import Data.Char
+import Data.List
+import Data.Ord
+import Text.ParserCombinators.ReadP
+-- import Data.Eq.HT
 
-module AOC.Common (
-  ) where
+comma = satisfy (',' ==)
 
+number = do
+  sign <- option ' ' (char '-')
+  magnitude <- many1 $ satisfy isDigit
+  return $ (read (sign : magnitude)) :: ReadP Integer
+
+int :: ReadP Int
+int = do
+  sign <- option ' ' (char '-')
+  magnitude <- many1 $ satisfy isDigit
+  return $ (read (sign : magnitude)) :: ReadP Int
+
+numberList = sepBy1 number comma
+
+parseMaybe :: ReadP a -> String -> Maybe a
+parseMaybe parser input =
+    case reverse $ readP_to_S parser input of
+        [] -> Nothing
+        ((result, _):_) -> Just result
+
+notSame :: Eq a => (a, a) -> Bool
+notSame (a, b) = a /= b
+
+oneAndNext :: [a] -> [(a,a)]
+oneAndNext sequence = zip sequence (tail sequence)
+
+untilStable :: Eq a => [a] -> a
+untilStable seq = if null pairs then head seq else snd $ last pairs
+  where pairs = takeWhile notSame $ oneAndNext seq
+
+replace :: Int -> a -> [a] -> [a]
+replace n x xs = (take n xs) ++ [x] ++ (drop (n+1) xs)
+
+chunks :: Int -> [a] -> [[a]]
+chunks n = takeWhile (not.null) . unfoldr (Just . splitAt n)
+
+sortOrder :: (a -> a -> Ordering) -> [a] -> [Int]
+sortOrder f list = map fst $ sortBy (\x y -> f (snd x) (snd y)) $ indexed list
+
+indexed :: [a] -> [(Int, a)]
+indexed list = zip [0..(length list)] list
+
+-- partitionOn f = (groupBy $ equating f) . (sortBy $ comparing f)
+
+cross :: Eq a => [a] -> [[(a,a)]]
+cross list = [[(a,b) | b<-list , a /= b] | a <- list]
+
+crossWith :: Eq a => (a -> a -> b) -> [a] -> [[b]]
+crossWith f list = [[f a b | b<-list , a /= b] | a <- list]
