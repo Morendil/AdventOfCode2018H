009d1d96d78faeedaec617c72c1c2949c40251c3
Laurent Bossavit <laurent.git@bossavit.com>
2020-01-04 15:12:55 +0100

    Day 8 part two

--- src/AOC/Challenge/Day08.hs
+++ src/AOC/Challenge/Day08.hs
@@ -14,16 +14,23 @@ module AOC.Challenge.Day08 (
 import AOC.Prelude
 import Data.Tree
 
+import Debug.Trace
+
 tree :: [Int] -> Tree [Int]
 tree = fst . goTree
   where goTree (0:nmeta:rest) = (Node (take nmeta rest) [], drop nmeta rest)
-        goTree (n:nmeta:rest) = (Node (take nmeta rest') children, drop nmeta rest')
+        goTree (n:nmeta:rest) = (Node (take nmeta rest') (reverse children), drop nmeta rest')
           where (children, rest') = (iterate goForest ([], rest)) !! n
         goForest (trees, rest) = (tree:trees, rest')
           where (tree, rest') = goTree rest
 
 sumMeta :: Tree [Int] -> Int
 sumMeta = sum . concat . flatten
+
+treeValue :: Tree [Int] -> Int
+treeValue = foldTree nodeValue
+  where nodeValue meta [] = sum meta
+        nodeValue meta childrenValue = sum $ map (\n -> if n-1 < length childrenValue then childrenValue !! (n-1) else 0) meta
 
 day08a :: [Int] :~> Int
 day08a = MkSol
@@ -32,9 +39,9 @@ day08a = MkSol
     , sSolve = Just . sumMeta . tree
     }
 
-day08b :: _ :~> _
+day08b :: [Int] :~> Int
 day08b = MkSol
-    { sParse = Just
+    { sParse = Just . map read . words
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . treeValue . tree
     }
