6672d766bf553a8487481a6cba90ae4a16b18fed
Laurent Bossavit <laurent.git@bossavit.com>
2020-01-08 22:58:31 +0100

    Fix Map

--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -13,15 +13,16 @@ module AOC.Challenge.Day11 (
 import AOC.Prelude hiding (toList, transpose)
 import Data.Function
 import Linear.V2
+import Data.Ix
 import qualified Data.Map as M
 
 type Point = V2 Int
 
 fuelMatrix :: Int -> Map Point Int
-fuelMatrix serial = foldr (\xy -> M.insert xy (power xy)) M.empty $ range ((V2 1 1),(V2 300 300))
+fuelMatrix serial = foldr (\xy -> M.insert xy (power serial xy)) M.empty $ range ((V2 1 1),(V2 300 300))
 
-power :: Int -> (Int, Int) -> Int
-power serial (y,x) = hundreds - 5
+power :: Int -> Point -> Int
+power serial (V2 x y) = hundreds - 5
   where hundreds = (powerLevel `div` 100) `mod` 10
         powerLevel = ((rackId * y) + serial) * rackId
         rackId = x + 10
@@ -34,21 +35,21 @@ display2 (x,y) = show x ++ "," ++ show y
 display3 (x,y,z) = show x ++ "," ++ show y ++ "," ++ show z
 
 summedAreaTable :: Map Point Int -> Map Point Int
-summedAreaTable mat = sat
+summedAreaTable mat = force sat
   where sat = M.mapWithKey rollUp mat
-        rollUp ((x,y) val) = sum $ catMaybes [Just val, above, left, (-diagonal)]
-          where above = M.lookup (x,y-1) sat
-                left = M.lookup (x-1,y) sat
-                diagonal = M.lookup (x-1,y-1) sat
+        rollUp (V2 x y) val = sum $ catMaybes [Just val, above, left, fmap negate diagonal]
+          where above = M.lookup (V2 x (y-1)) sat
+                left = M.lookup (V2 (x-1) y) sat
+                diagonal = M.lookup (V2 (x-1) (y-1)) sat
 
 allSquares :: Map Point Int -> [((Int,Int,Int),Int)]
-allSquares mat = [((x,y,side), sumAt (x,y,side)) | side <-[1..nrows mat], x <- [1..(300 - side - 1)], y <- [1..(300 - side - 1)]]
-  where sumAt (x,y,1) = fromMaybe 0 $ M.lookup (x,y) mat
+allSquares mat = [((x,y,side), sumAt (x,y,side)) | side <-[1..300], x <- [1..(300 - side - 1)], y <- [1..(300 - side - 1)]]
+  where sumAt (x,y,1) = fromMaybe 0 $ M.lookup (V2 x y) mat
         sumAt (x,y,side) = topLeft+bottomRight-bottomLeft-topRight
-          where bottomRight = fromMaybe 0 $ M.lookup (x+side-1,y+side-1) mat
-                topLeft = fromMaybe 0 $ M.lookup (x-1,y-1) mat
-                bottomLeft = fromMaybe 0 $ M.lookup (x-1,y+side-1) mat
-                topRight = fromMaybe 0 $ M.lookup (x+side-1,y-1) mat
+          where bottomRight = fromMaybe 0 $ M.lookup (V2 (x+side-1) (y+side-1)) mat
+                topLeft = fromMaybe 0 $ M.lookup (V2 (x-1) (y-1)) mat
+                bottomLeft = fromMaybe 0 $ M.lookup (V2 (x-1) (y+side-1)) mat
+                topRight = fromMaybe 0 $ M.lookup (V2 (x+side-1)( y-1)) mat
 
 day11a :: Int :~> String
 day11a = MkSol
